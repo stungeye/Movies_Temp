3cd0fb51fc7db3cc3e913611100301564e0813e0
Kyle Geske <stungeye@gmail.com>
2020-09-30 16:40:56 -0500

    Implemented a home page with an associate route and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
   # GET /movies      =>  movies#index
   # GET /movies/:id  =>  movies#show
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
+
+  # GET /   => home#index
+  root to: "home#index"
 end
