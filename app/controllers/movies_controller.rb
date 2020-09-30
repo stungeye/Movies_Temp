1d5c1f7fb598c35fa1bf91b0b379366ba93a53c4
Kyle Geske <stungeye@gmail.com>
2020-09-30 15:26:43 -0500

    Basic implementations for the Movies controller index and show actions/views.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,15 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.order("average_vote DESC")
+    @movies = Movie.includes(:production_company).order("average_vote DESC")
   end
+  # The @movies variable will be automatically shared with the associated view:
+  # /app/views/movies/index.html.erb
 
-  def show; end
+  # The route to this action has a :id in it. GET /movies/:id
+  # And that id ends up in the params hash like this: params[:id]
+  def show
+    @movie = Movie.find(params[:id])
+  end
+  # The @movie variable will be automatically shared with the associated view:
+  # /app/views/movies/show.html.erb
 end
