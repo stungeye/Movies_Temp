98e9ff925f1aa7898b979acbae4638e39d9e8bc0
Kyle Geske <stungeye@gmail.com>
2020-09-30 15:28:07 -0500

    Generated a production companies controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'production_companies/index'
+  get 'production_companies/show'
   # GET /movies      =>  movies#index
   # GET /movies/:id  =>  movies#show
   resources :movies, only: %i[index show]
