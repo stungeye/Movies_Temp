1a56d0ef2c4fe5ef8ee44f7910bafaf728e64d5a
Kyle Geske <stungeye@gmail.com>
2020-09-30 16:08:42 -0500

    Further implementation of the show and index views for movies and production companies.

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,19 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    @production_companies = ProductionCompany.all
   end
+  # In the associated index view:
+  # - List all the companies in an unordered list with each being a link to the show page.
 
   def show
+    @production_company = ProductionCompany.find(params[:id])
   end
+  # In the associated show view:
+  # - Display the name of the Production Company.
+  # - Display a list of links to all the movies made by this production company.
 end
+
+# Meanwhile, over at the movie show page:
+# - Display the other movie details we have available. (description, year, duration, etc)
+# - Display the name of the production company that made the movie.
+# - Ensure that this name is also a link to the production company's show page.
