3476661cd8bce52ae7648fb93308cbda4edbe653
Kyle Geske <stungeye@gmail.com>
2020-10-02 08:09:17 -0500

    Properly ordering the production companies on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
                    .order("average_vote DESC")
                    .limit(10)
 
-    @production_companies = ProductionCompany.limit(10)
+    @production_companies = ProductionCompany.left_joins(:movies)
+                                             .group("production_companies.id")
+                                             .select("production_companies.*")
+                                             .select("count(movies.id) as movie_count")
+                                             .order("movie_count DESC")
+                                             .limit(10)
   end
 end
