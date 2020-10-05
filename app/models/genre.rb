58cf80cff2b8cf1744cb103a0e72076ac03ad5c6
Kyle Geske <stungeye@gmail.com>
2020-10-05 15:23:03 -0500

    Added the has many through associations to movies and genres.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,8 @@
 class Genre < ApplicationRecord
+  has_many :movie_genres
+  has_many :movies, through: :movie_genres
+
+  validates :name, presence: true
+
+  # genre_object.movies
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,10 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  # movie_object.production_company
+  # movie_object.genres
 
   validates :title, :year, :duration, :description, :average_vote, presence: true
   validates :title, uniqueness: true
